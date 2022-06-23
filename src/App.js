87659da2d74e3c4e1eb7d8d7b75ea2b8c8bb7271
AayushiTalwar <aayushitalwar02@gmail.com>
2022-06-23 21:31:01 +0530

    added more components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
-import Header from './Header.js'
 import './App.css';
+import Home from './components/Home';
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import Login from './components/Login';
+import { AuthProvider } from "./contexts/AuthContext";
+import PrivateRoute from './PrivateRoute';
+import Header from './components/Header';
+import Checkout from './components/Checkout';
 
 function App() {
   return (
-    <div className="App">
-      <h1> hello there! i am the amazon clone app!</h1>
-      {/* Header */}
-      <Header />
-      {/* Home  */}
-    
-    </div>
+      <div className="app">
+      <Router>
+        <AuthProvider>
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <PrivateRoute exact path="/" component={Home} />
+          <Route path="/checkout">
+            <Header />
+            <Checkout />
+          </Route>
+        </Switch>
+        </AuthProvider>
+      </Router>
+      </div>
   );
 }
 
--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,21 @@
+import firebase from "firebase";
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyB6Rw0lDpquloVk576nUVN8qSLliEOEEys",
-    authDomain: "clone2022-6a1e0.firebaseapp.com",
-    projectId: "clone2022-6a1e0",
-    storageBucket: "clone2022-6a1e0.appspot.com",
-    messagingSenderId: "95250566612",
-    appId: "1:95250566612:web:43269a76108e000fc0da41",
-    measurementId: "G-3XQKNB6F93"
-  };
+  apiKey: "AIzaSyB6Rw0lDpquloVk576nUVN8qSLliEOEEys",
+  authDomain: "clone2022-6a1e0.firebaseapp.com",
+  projectId: "clone2022-6a1e0",
+  storageBucket: "clone2022-6a1e0.appspot.com",
+  messagingSenderId: "95250566612",
+  appId: "1:95250566612:web:43269a76108e000fc0da41",
+  measurementId: "G-3XQKNB6F93"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+const storage = firebase.storage();
+
+export { auth, provider, storage };
+export default db;
