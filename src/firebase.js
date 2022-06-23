80cd1771f3048d48f89fae1623332803fbe9b7f0
AayushiTalwar <aayushitalwar02@gmail.com>
2022-06-23 20:43:42 +0530

    Initialize project using Create React App

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyB6Rw0lDpquloVk576nUVN8qSLliEOEEys",
+    authDomain: "clone2022-6a1e0.firebaseapp.com",
+    projectId: "clone2022-6a1e0",
+    storageBucket: "clone2022-6a1e0.appspot.com",
+    messagingSenderId: "95250566612",
+    appId: "1:95250566612:web:43269a76108e000fc0da41",
+    measurementId: "G-3XQKNB6F93"
+  };
